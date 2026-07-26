6e5d9913cf30d4302fec889673d5a7008dff6cc0
vyash0048-bit <vyash0048@gmail.com>
2026-07-26 19:16:50 +0530

    Add project setup files, requirements, and initial implementation for Toxic Comment Classifier

---
+++ src/ToxicCommentClassifier/utils/common.py
@@ -0,0 +1,126 @@
+import os
+import sys
+import yaml
+import json
+import joblib
+from ensure import ensure_annotations
+from box import ConfigBox
+from box.exceptions import BoxValueError
+from pathlib import Path
+from typing import Any
+from src.ToxicCommentClassifier.logger import logger
+from src.ToxicCommentClassifier.exception import CustomException
+
+@ensure_annotations
+def read_yaml(path_to_yaml: Path) -> ConfigBox:
+    """reads yaml file and returns
+    
+    Args:
+        path_to_yaml (Path): path like input
+        
+    Raises:
+        ValueError: if yaml file is empty
+        e: empty file
+        
+    Returns:
+        ConfigBox: ConfigBox type
+    """
+    try:
+        with open(path_to_yaml) as yaml_file:
+            content = yaml.safe_load(yaml_file)
+            logger.info(f"yaml file: {path_to_yaml} loaded successfully")
+            return ConfigBox(content)
+    except BoxValueError:
+        raise ValueError("yaml file is empty")
+    except Exception as e:
+        raise CustomException(e, sys)
+
+@ensure_annotations
+def create_directories(path_to_directories: list, verbose=True):
+    """create list of directories
+    
+    Args:
+        path_to_directories (list): list of path of directories
+        verbose (bool, optional): ignore if multiple dirs is to be created. Defaults to False.
+    """
+    for path in path_to_directories:
+        os.makedirs(path, exist_ok=True)
+        if verbose:
+            logger.info(f"created directory at: {path}")
+
+@ensure_annotations
+def save_json(path: Path, data: dict):
+    """save json data
+    
+    Args:
+        path (Path): path to json file
+        data (dict): data to be saved in json file
+    """
+    try:
+        with open(path, "w") as f:
+            json.dump(data, f, indent=4)
+        logger.info(f"json file saved at: {path}")
+    except Exception as e:
+        raise CustomException(e, sys)
+
+@ensure_annotations
+def load_json(path: Path) -> ConfigBox:
+    """load json files data
+    
+    Args:
+        path (Path): path to json file
+        
+    Returns:
+        ConfigBox: data as class attributes instead of dict
+    """
+    try:
+        with open(path) as f:
+            content = json.load(f)
+        logger.info(f"json file loaded successfully from: {path}")
+        return ConfigBox(content)
+    except Exception as e:
+        raise CustomException(e, sys)
+
+@ensure_annotations
+def save_bin(data: Any, path: Path):
+    """save binary file
+    
+    Args:
+        data (Any): data to be saved as binary
+        path (Path): path to binary file
+    """
+    try:
+        joblib.dump(value=data, filename=path)
+        logger.info(f"binary file saved at: {path}")
+    except Exception as e:
+        raise CustomException(e, sys)
+
+@ensure_annotations
+def load_bin(path: Path) -> Any:
+    """load binary data
+    
+    Args:
+        path (Path): path to binary file
+        
+    Returns:
+        Any: object stored in the file
+    """
+    try:
+        data = joblib.load(path)
+        logger.info(f"binary file loaded from: {path}")
+        return data
+    except Exception as e:
+        raise CustomException(e, sys)
+
+@ensure_annotations
+def get_size(path: Path) -> str:
+    """get size in KB
+    
+    Args:
+        path (Path): path of the file
+        
+    Returns:
+        str: size in KB
+    """
+    size_in_kb = round(os.path.getsize(path)/1024)
+    return f"~ {size_in_kb} KB"
